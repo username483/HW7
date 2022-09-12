2f98499ba0c2a6f014a0e23bf565a401986161df
Olga Korchagina <olga_korchagina_2016@mail.ru>
2022-09-12 23:35:02 +0300

    задача на вывод среднего арифметического столбца двумерного массива

---
+++ Task3/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Clear();
+Console.Write("Введите количество строк m: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов n: ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[m, n];
+
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+FindAverage(matrix);
+
+void FindAverage(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[i, j];
+
+        }
+        Console.WriteLine($"Cреднее арифметическое элементов столбца {j + 1} = {(double)sum / m:F1}");
+    }
+}
+
+//как вывести значения в строку, как в условии?
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++) // 0 это количество строк
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // 1 это количество столбцов
+        {
+            Console.Write($" {matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+
